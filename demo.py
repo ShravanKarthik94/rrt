616e34319188f37038b1a29a002c3a6e20ad29bd
Shravan <shravan@Gabes-Air.lan1>
2019-11-14 08:58:28 -0500

    birrt working

--- demo.py
+++ demo.py
@@ -6,6 +6,7 @@ import time
 import argparse
 from collections import defaultdict 
 import copy
+import math
 
 UR5_JOINT_INDICES = [0, 1, 2]
 stp_size = 0.005
@@ -82,6 +83,20 @@ def findPath(s, d, vertex, adjList, visited, currPath):
     currPath.pop()
     visited[idx] = 0
 
+def checkconnection(v1, v2):
+    n1 = math.inf
+    n2 = math.inf
+    len1 = len(v1)
+    len2 = len(v2)
+    for x in range(len1):
+        for y in range(len2):
+            dist = np.linalg.norm(np.array(v1[x])-np.array(v2[y]))
+            if(dist <= step):
+                n1 = v1[x]
+                n2 = v2[y]
+                return n1, n2
+    return n1, n2
+
 def rrt():
     path = []
     edges = []
@@ -109,7 +124,7 @@ def rrt():
                 print(np.linalg.norm(gp-newpt))
                 break
         cntr = cntr + 1
-    print("Done",cntr,len(path))
+    print("Done",cntr,len(path), len(edges))
     adjList = buildAdjList(edges, path)
     visited = np.zeros(len(path))
     currPath = []
@@ -117,11 +132,71 @@ def rrt():
     print("Done with everything")
 
 def birrt():
-    #################################################
-    # TODO your code to implement the birrt algorithm
-    #################################################
-    pass
+    path1 = []
+    edges1 = []
+    path2 = []
+    edges2 = []
+    path1.append(start_conf)
+    path2.append(goal_conf)
+    cntr = 1
+    gp = np.array(goal_conf)
+    sp = np.array(start_conf)
+    while True:
+        xnew = np.random.uniform(low=-1, high=1)
+        ynew = np.random.uniform(low=-1, high=1)
+        znew = np.random.uniform(low=-1, high=1)
+        xnew2 = np.random.uniform(low=-1, high=1)
+        ynew2 = np.random.uniform(low=-1, high=1)
+        znew2 = np.random.uniform(low=-1, high=1)
+        pt2s = np.array((xnew, ynew, znew))
+        pt2e = np.array((xnew2, ynew2, znew2))
+        if cntr % biasCount == 0:
+            pt2s = gp
+            pt2e = sp
+        nearestPt = find_nn(path1, pt2s)
+        nearestPt2 = find_nn(path2, pt2e)
+        addp1 = 1
+        addp2 = 1
+       
+        cntr = cntr + 1
 
+        if(np.linalg.norm(nearestPt - pt2s) == 0.0):
+            addp1 = 0
+        if(np.linalg.norm(nearestPt2 - pt2e) == 0.0):
+            addp2 = 0
+       
+        if addp1==1 :
+            newpt = move_node(nearestPt, pt2s)
+            if not collision_fn(newpt):
+                path1.append(tuple(newpt.reshape(1,-1)[0]))
+                edges1.append(((tuple(nearestPt.reshape(1,-1)[0])),tuple(newpt.reshape(1,-1)[0])))
+        
+        if addp2==1 :
+            newpt2 = move_node(nearestPt2, pt2e)
+            if not collision_fn(newpt2):
+                path2.append(tuple(newpt2.reshape(1,-1)[0]))
+                edges2.append(((tuple(nearestPt2.reshape(1,-1)[0])),tuple(newpt2.reshape(1,-1)[0])))
+        
+        if addp1  == 1 or addp2 == 1:
+            nd1 , nd2 = checkconnection(path1, path2)
+            if nd1 == math.inf or nd2 == math.inf:
+                continue
+            else:
+                path1.append(nd2)
+                edges1.append((nd1,nd2))
+                for x in range(len(path2)):
+                    if not (path2[x] == nd2):
+                        path1.append(path2[x])
+                    if x < len(edges2):
+                        edges1.append(edges2[x])
+                break
+    
+    print("Done",cntr,len(path1))
+    adjList = buildAdjList(edges1, path1)
+    visited = np.zeros(len(path1))
+    currPath = []
+    findPath(start_conf, goal_conf, path1, adjList, visited, currPath)
+    print("Done with everything")
 
 def birrt_smoothing():
     ################################################################
@@ -177,10 +252,12 @@ if __name__ == "__main__":
     if args.birrt:
         if args.smoothing:
             # using birrt with smoothing
-            path_conf = birrt_smoothing()
+            birrt_smoothing()
+            path_conf = finalPath
         else:
             # using birrt without smoothing
-            path_conf = birrt()
+            birrt()
+            path_conf = finalPath
     else:
         # using rrt
         rrt()
